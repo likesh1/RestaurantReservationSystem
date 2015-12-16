9d28aeb61eb0caeb6070e7284c1ce321a865727d
Manoj <manoj.pammina.gre@gmail.com>
2015-12-16 18:16:29 -0500

    feat(init): added all the files for the new project

---
+++ web/scripts/myscripts.js
@@ -0,0 +1,25 @@
+/**
+ * Created by manoj on 12/15/15.
+ */
+var vid = document.getElementById("bgvid")
+pauseButton = document.getElementById("vidpause");
+function vidFade() {
+    vid.classList.add("stopfade");
+}
+vid.addEventListener('ended', function() {
+    // only functional if "loop" is removed
+    vid.pause();
+    // to capture IE10
+    vidFade();
+});
+
+pauseButton.addEventListener("click", function() {
+    vid.classList.toggle("stopfade");
+    if (vid.paused) {
+        vid.play();
+        pauseButton.innerHTML = "Pause";
+    } else {
+        vid.pause();
+        pauseButton.innerHTML = "Paused";
+    }
+});
